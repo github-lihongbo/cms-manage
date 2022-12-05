6a46ed86b26a4b02ea04c93a9ee9eeea6dd8077c
李鸿博 <2874667276@qq.com>
2022-12-05 16:38:43 +0800

    解包前

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from 'antd';
+import { Outlet } from 'react-router-dom';
+
+const App = () => (
+  <div className="App">
+    <Outlet />
+  </div>
+);
+
+export default App;
---
+++ src/pages/Means.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Means() {
+  return (
+    <div>详细</div>
+  )
+}
